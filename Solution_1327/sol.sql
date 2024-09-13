0363fdc6ddea56acdc0358503fc9088b47ddd329
Yash Wasankar <yashwasankar008@gmail.com>
2024-09-13 12:34:41 +0530

    Solution : 1327. List the Products Ordered in a Period

---
+++ Solution_1327/sol.sql
@@ -0,0 +1,7 @@
+SELECT product_name, SUM(unit) AS unit
+FROM
+    Orders AS o
+    JOIN Products AS p ON o.product_id = p.product_id
+WHERE DATE_FORMAT(order_date, '%Y-%m') = '2020-02'
+GROUP BY o.product_id
+HAVING unit >= 100;
