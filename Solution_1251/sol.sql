bad3414b5d9e4194c1bb470ddb77740712083c60
Yash Wasankar <yashwasankar008@gmail.com>
2024-09-13 13:12:11 +0530

    Solution : 1251. Average Selling Price

---
+++ Solution_1251/sol.sql
@@ -0,0 +1,8 @@
+SELECT
+    p.product_id,
+    IFNULL(ROUND(SUM(price * units) / SUM(units), 2), 0) AS average_price
+FROM
+    Prices AS p
+    LEFT JOIN UnitsSold AS u
+        ON p.product_id = u.product_id AND purchase_date BETWEEN start_date AND end_date
+GROUP BY 1;
