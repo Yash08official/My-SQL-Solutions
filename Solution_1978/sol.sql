8e601021feb15c600795d608c57eba8e123a6bcc
Yash Wasankar <yashwasankar008@gmail.com>
2024-09-15 16:34:34 +0530

    Solution : 1978. Employees Whose Manager Left the Company

---
+++ Solution_1978/sol.sql
@@ -0,0 +1,6 @@
+SELECT e1.employee_id
+FROM
+    Employees AS e1
+    LEFT JOIN Employees AS e2 ON e1.manager_id = e2.employee_id
+WHERE e1.salary < 30000 AND e1.manager_id IS NOT NULL AND e2.employee_id IS NULL
+ORDER BY 1;
