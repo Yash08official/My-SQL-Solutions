675c5ebbdc8eb8a9e875a2e6eb700d0b4db682de
Yash Wasankar <yashwasankar008@gmail.com>
2024-09-16 03:06:17 +0530

    Solution : 570. Managers with at Least 5 Direct Reports

---
+++ Solution_570/sol.sql
@@ -0,0 +1,10 @@
+SELECT name
+FROM
+    Employee
+    JOIN (
+        SELECT managerId AS id, COUNT(1) AS cnt
+        FROM Employee
+        GROUP BY 1
+        HAVING cnt >= 5
+    ) AS t
+        USING (id);
