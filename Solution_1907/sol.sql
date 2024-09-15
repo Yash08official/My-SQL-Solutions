c907d4bcafa89dc34f58239001af6454fa572614
Yash Wasankar <yashwasankar008@gmail.com>
2024-09-15 11:38:31 +0530

    Solution : 1907. Count Salary Categories

---
+++ Solution_1907/sol.sql
@@ -0,0 +1,8 @@
+SELECT 'Low Salary' AS category, IFNULL(SUM(income < 20000), 0) AS accounts_count FROM Accounts
+UNION
+SELECT
+    'Average Salary' AS category,
+    IFNULL(SUM(income BETWEEN 20000 AND 50000), 0) AS accounts_count
+FROM Accounts
+UNION
+SELECT 'High Salary' AS category, IFNULL(SUM(income > 50000), 0) AS accounts_count FROM Accounts;
