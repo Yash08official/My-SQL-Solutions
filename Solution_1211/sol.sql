eb8c8af77f0f2b0d1fc87d6a5905bff0d4596aed
Yash Wasankar <yashwasankar008@gmail.com>
2024-09-17 04:13:58 +0530

    Solution : 1211. Queries Quality and Percentage

---
+++ Solution_1211/sol.sql
@@ -0,0 +1,7 @@
+SELECT
+    query_name,
+    ROUND(AVG(rating / position), 2) AS quality,
+    ROUND(AVG(rating < 3) * 100, 2) AS poor_query_percentage
+FROM Queries
+WHERE query_name IS NOT NULL
+GROUP BY 1;
